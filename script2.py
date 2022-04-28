34885e6173a60e66dad3a6ef66af1409d520cb93
AaronMonkey <lequoc287@gmail.com>
2022-04-28 14:41:58 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,48 +1,15 @@
-import paramiko
+import tkinter as tk
 
-switch_ip = "192.168.255.253"
-switch_username = "admin"
-switch_password = "admin"
+root= tk.Tk()
 
-ssh = paramiko.SSHClient()
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
 
+def hello ():  
+    label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    
+button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
+canvas1.create_window(150, 150, window=button1)
 
-def run_command_on_device(ip_address, username, password, command):
-    """ Connect to a device, run a command, and return the output."""
-
-    ssh.load_system_host_keys() #keys loaded 
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #If needed add key function 
-
-    total_attempts = 3
-    for attempt in range(total_attempts):
-        try:
-            print("Attempt to connect: %s" % attempt) #router connection 
-            ssh.connect(switch_ip,
-                        username=switch_username,
-                        password=switch_password,
-                        look_for_keys=False)
-            # Run command.
-            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
-            # Read output from command.
-            output = ssh_stdout.readlines()
-            # Close connection.
-            ssh.close()
-            return output
-
-        except Exception as error_message:
-            print("Unable to connect")
-            print(error_message)
-
-print("Got to line 36")
-# Run function
-switch_output = run_command_on_device(
-    switch_ip, switch_username, switch_password, "enable port 4")
-
-# Analyze show ip route output
-# Make sure we didn't receive empty output.
-if switch_output != None:
-    for line in switch_output:
-        if 'str' in line:
-            print("Default Line given:")
-            print(line)
-print("Finished program")
+root.mainloop()
